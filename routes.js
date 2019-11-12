2aa329d14b6a2c5747a1eea7de290f75835d0b36
GuilhermeCunha <guiscunha@gmail.com>
2019-11-11 21:50:17 -0300

    Put + Delete

--- routes.js
+++ routes.js
@@ -8,8 +8,8 @@ const UserController = require('./src/Controllers/UserController');
 routes.get('/listar-usuarios', UserController.listarUsuarios);
 
 routes.post('/criar-usuario', UserController.criarUsuario);
-routes.post('/remover-usuario', UserController.removerUsuario);
-routes.post('/editar-usuario', UserController.editarUsuario);
+routes.delete('/remover-usuario', UserController.removerUsuario);
+routes.put('/editar-usuario', UserController.editarUsuario);
 routes.post('/login', UserController.login);
 
 module.exports = routes;
